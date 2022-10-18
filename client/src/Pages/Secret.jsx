ce85ac985e3a90b145b0aa65d7396591599609f8
somesh123-ctrl <55444715+somesh123-ctrl@users.noreply.github.com>
2022-10-18 18:55:05 +0530

    Add files via upload

---
+++ client/src/Pages/Secret.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import Canvas from "../Components/Canvas";
+
+const Secret = () => {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  useEffect(() => {
+    const verifyUser = async () => {
+      if (!cookies.jwt) {
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "/",
+          {},
+          { withCredentials: true }
+        );
+        if (!data.status) {
+          removeCookie("jwt");
+          navigate("/login");
+        } else
+          toast(`Hi ${data.user}`, { theme: "dark", position: "bottom-right" });
+      }
+    };
+    verifyUser();
+  }, [navigate, cookies, removeCookie]);
+
+  const logout = () => {
+    removeCookie("jwt");
+    navigate("/register");
+  };
+
+  return (
+    <>
+      <div className="App">
+        <nav>
+          <button onClick={logout}>Logout</button>
+        </nav>
+        <div className="canvas">
+          <Canvas width={1024} height={650} />
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Secret;
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./Routes/Auth.js");
+const cookieParser = require("cookie-parser");
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Troubleshooting...");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running at port http://localhost:${PORT}/`)
+);
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Connected to database successfully.`))
+  .catch((error) => console.log(error.message));
+
+  if(process.env.NODE_ENV ==='production') {
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+     
+        res.sendFile(path.resolve(__dirname , 'client' , 'build', 'index.html'));
+
+    });
+}
+
+app.use(
+  cors({
+    origin: [`http://localhost:3000`],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", authRoutes);
+
